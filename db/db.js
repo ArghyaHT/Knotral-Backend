a310b0f7eef10bdccf659d19c75e0e87bf08716b
Arghya211994 <arghya@himanstech.com>
2026-01-28 12:26:55 +0530

    db connection removed

--- db/db.js
+++ db/db.js
@@ -7,9 +7,7 @@ dotenv.config();
 const connectDB = async() =>{
     try{
         const conn = await mongoose.connect(process.env.MONGODB_URI)
-
-        // const conn = await mongoose.connect("mongodb+srv://KnotralTrainingDemo:EvZtK9cRf77Ybvmh@knotraltrainingdemo.clxzhxw.mongodb.net/")
-
+        
         console.log(`MongoDB is successfully connected: ${conn.connection.host}`)
     }
     catch(error){
