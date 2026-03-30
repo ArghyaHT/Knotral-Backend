32ab85593a8daf1f545838affc04d16dcb344b97
Arghya211994 <arghya@himanstech.com>
2026-03-30 17:11:26 +0530

    cookie config chnaged

--- controller/userController.js
+++ controller/userController.js
@@ -299,13 +299,14 @@ export const loginUser = async (req, res, next) => {
     // Remove password from response
     const { password: _, ...userData } = user.toObject();
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: false,
-      sameSite: "lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    });
-    
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,        // REQUIRED for cross-site cookies
+  sameSite: "none",    // REQUIRED for cross-domain
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  path: "/"
+});
+
     return res.status(200).json({
       success: true,
       message: "Login successful",
