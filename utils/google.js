66a59e55e7e3424752a1c0677984822bda3bd721
Arghya211994 <arghya@himanstech.com>
2026-04-15 15:40:23 +0530

    signup uri fixed

--- utils/google.js
+++ utils/google.js
@@ -2,21 +2,22 @@ export const GOOGLE_CONFIG = {
   clientId: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   redirectUri: process.env.GOOGLE_CALLBACK_URL,
+  signupRedirectUri: process.env.GOOGLE_SIGNUP_CALLBACK_URL,
 };
 
 // ✅ Create OAuth Client
 export const getOAuthClient = () => {
   if (
-    !GOOGLE_CONFIG.process.env.GOOGLE_CLIENT_ID ||
-    !GOOGLE_CONFIG.process.env.GOOGLE_CLIENT_SECRET ||
-    !GOOGLE_CONFIG.process.env.GOOGLE_SIGNUP_CALLBACK_URL
+    !GOOGLE_CONFIG.clientId ||
+    !GOOGLE_CONFIG.clientSecret ||
+    !GOOGLE_CONFIG.signupRedirectUri
   ) {
     throw new Error("Google OAuth environment variables are missing");
   }
 
   return new google.auth.OAuth2(
-    GOOGLE_CONFIG.process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CONFIG.process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_CONFIG.process.env.GOOGLE_SIGNUP_CALLBACK_URL
+    GOOGLE_CONFIG.clientId,
+    GOOGLE_CONFIG.clientSecret,
+    GOOGLE_CONFIG.signupRedirectUri
   );
 };
