9e85bd861d47ab8e1fea484decadd23bb97d005c
Arghya211994 <arghya@himanstech.com>
2026-04-30 11:10:15 +0530

    forget password otp api added

--- controller/sendAndVerifyOtpController.js
+++ controller/sendAndVerifyOtpController.js
@@ -13,21 +13,89 @@ export const sendOtp = async (req, res) => {
 
     const existingUser = await Users.findOne({ email });
 
-   if (existingUser) {
-  if (existingUser.authType === "google") {
-    return res.status(400).json({
-      success: false,
-      message: "Email already registered with Google",
-    });
-  }
+    if (existingUser) {
+      if (existingUser.authType === "google") {
+        return res.status(400).json({
+          success: false,
+          message: "Email already registered with Google",
+        });
+      }
 
-  if (existingUser.authType === "local") {
-    return res.status(400).json({
-      success: false,
-      message: "Email already registered with email and password",
+      if (existingUser.authType === "local") {
+        return res.status(400).json({
+          success: false,
+          message: "Email already registered with email and password",
+        });
+      }
+    }
+
+    const otp = otpGenerator.generate(4, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      specialChars: false,
+      digits: true,
     });
+
+    // 🔥 Expiry (5 minutes)
+    const otpExpires = new Date(Date.now() + 5 * 60 * 1000);
+
+    // 🔥 Check if already exists
+    let user = await TempUsers.findOne({ email });
+
+    if (user) {
+      // Optional: rate limit (30 sec)
+      const diff = Date.now() - new Date(user.updatedAt).getTime();
+      if (diff < 30 * 1000) {
+        return res.json({
+          success: false,
+          message: "Please wait before requesting another OTP",
+        });
+      }
+
+      // Update existing record
+      user.otp = otp;
+      user.otpExpires = otpExpires;
+      user.isVerified = false;
+      await user.save();
+    } else {
+      // Create new record
+      await TempUsers.create({
+        email,
+        otp,
+        otpExpires,
+      });
+    }
+
+    await sendOtpEmail(email, otp)
+
+
+    res.json({ success: true, message: "OTP sent successfully" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false });
   }
-}
+};
+
+
+export const sendForgetPasswordOtp = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.json({ success: false, message: "Email is required" });
+    }
+
+    const existingUser = await Users.findOne({ email });
+
+    if (existingUser) {
+      if (existingUser.authType === "google") {
+        return res.status(400).json({
+          success: false,
+          message: "Email registered with Google",
+        });
+      }
+    }
 
     const otp = otpGenerator.generate(4, {
       upperCaseAlphabets: false,
--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { sendOtp, verifyOtp } from "../controller/sendAndVerifyOtpController.js";
+import { sendForgetPasswordOtp, sendOtp, verifyOtp } from "../controller/sendAndVerifyOtpController.js";
 
 const router = express.Router();
 
 // 🔥 Send OTP
 router.post("/send-otp", sendOtp);
+
+router.post("/send-forget-password-otp", sendForgetPasswordOtp);
+
 
 // 🔥 Verify OTP
 router.post("/verify-otp", verifyOtp);
